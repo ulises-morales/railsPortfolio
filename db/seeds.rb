2c1e2bb1479cef342a5aaf74837e8376aaf4260b
ulises-morales <ulises.m.025@gmail.com>
2017-04-14 22:10:03 -0700

    Added portfolio items via Resource generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do | blog |
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Bacon ipsum dolor amet boudin hamburger tongue tenderloin picanha porchetta frankfurter corned beef tri-tip pork loin swine drumstick. Pastrami pork chop leberkas, shank flank sirloin ground round pork shankle meatball. Hamburger corned beef flank, ball tip strip steak pig salami brisket kevin andouille tenderloin. Shoulder venison jerky tenderloin spare ribs biltong. Short ribs hamburger leberkas strip steak, ribeye shank corned beef pork chop meatloaf jowl biltong drumstick brisket capicola ground round. Hamburger shoulder kevin strip steak. Ball tip capicola turkey cupim meatball ham chicken corned beef picanha kevin strip steak boudin hamburger."
+  )
+end
+
+put "10 blog posts created"
+
+
+5.times do | skill |
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+
+put "5 skills created"
+
+
+put "9 portfolio items created"
+
+
+
+
+
+
+
