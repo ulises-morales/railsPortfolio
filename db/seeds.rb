896ca94f4d9821f040f445b3a5b1ce78998624a0
ulises-morales <ulises.m.025@gmail.com>
2017-04-14 22:31:53 -0700

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do | blog |
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Bacon ipsum dolor amet boudin hamburger tongue tenderloin picanha porchetta frankfurter corned beef tri-tip pork loin swine drumstick. Pastrami pork chop leberkas, shank flank sirloin ground round pork shankle meatball. Hamburger corned beef flank, ball tip strip steak pig salami brisket kevin andouille tenderloin. Shoulder venison jerky tenderloin spare ribs biltong. Short ribs hamburger leberkas strip steak, ribeye shank corned beef pork chop meatloaf jowl biltong drumstick brisket capicola ground round. Hamburger shoulder kevin strip steak. Ball tip capicola turkey cupim meatball ham chicken corned beef picanha kevin strip steak boudin hamburger."
+  )
+end
+
+puts "10 blog posts created"
+
+
+5.times do | skill |
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+
+puts "5 skills created"
+
+9.times do | portfolio_item |
+  MyPortfolio.create!(
+    title: "Portfolio title #{portfolio_item}",
+    subtitle: "My service",
+    body: "Bacon ipsum dolor amet boudin hamburger tongue tenderloin picanha porchetta frankfurter corned beef tri-tip pork loin swine drumstick. Pastrami pork chop leberkas, shank flank sirloin ground round pork shankle meatball. Hamburger corned beef flank, ball tip strip steak pig salami brisket kevin andouille tenderloin.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200",
+  )
+end
+
+puts "9 portfolio items created"
+
+
+
+
+
+
+
